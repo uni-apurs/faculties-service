f224334a0a80a2c4ed4fe003df6c406563e3aa13
Stefan Katic <katicstefan.coder@gmail.com>
2021-06-21 19:04:10 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/facultiesservice/FacultiesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.facultiesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultiesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurs/microservices/facultiesservice/FacultiesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.facultiesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultiesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
